5f82af8e3333f3dcd0891903d31602bd56a89897
huynhthanhthao <92310679+huynhthanhthao@users.noreply.github.com>
2024-03-22 09:35:41 +0700

    fix / backup data api

--- src/services/common.service.js
+++ src/services/common.service.js
@@ -7,38 +7,15 @@ class CommonService {
 
     static async backup(account) {
         // delete db.sequelize.models.School;
-        // const tables = Object.keys(db.sequelize.models);
-
-        const tables = [
-            "Role",
-            "GroupRole",
-            // "Account",
-            // "Book",
-            // "BookRequest",
-            // "BookStatus",
-            // "Category",
-            // "Class",
-            // "ClassHasUser",
-            // "DetailPenaltyTicket",
-            // "Field",
-            // "FieldHasBook",
-            // "FinePolicy",
-            // "FinePolicyHasBook",
-            // "GroupRole",
-            // "Language",
-            // "Role",
-            // "LoanReceipt",
-            // "PenaltyTicket",
-            // "Position",
-            // "Publisher",
-            // "ReaderGroup",
-        ];
+        const tables = Object.keys(db.sequelize.models).filter(
+            (table) => table != "School" && table != "Role" && table != "GroupRole"
+        );
 
         const queryPromises = tables.map(async (table) => {
             const data = await db[table].findAll({
-                // where: {
-                //     schoolId: account.schoolId,
-                // },
+                where: {
+                    schoolId: account.schoolId,
+                },
             });
             return { table, data: data.map((entry) => entry.toJSON()) };
         });
